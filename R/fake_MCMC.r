87f477c3478bd1475ce5bca49339684095620523
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2025-04-10 18:52:32 -0300

    adding the main function O.o

--- R/fake_MCMC.r
+++ R/fake_MCMC.r
@@ -1,11 +1,23 @@
+#' Generate 'fake' MCMC draws from a latent gaussian autorregressive order 1 (LNAR1) process
+#'
+#' @param N number of draws
+#' @param lmean log mean
+#' @param lsd log standard deviation
+#' @param phi correlation parameter of the latent AR(1)
+#' @param LN whether to generate from a log-normal (default is true)
+#'
+#' @returns a vector of size N with the draws
+#' @export generate_fake_MCMC
+#'
 generate_fake_MCMC <- function(N = 1E4,
-                               m = 0,
-                               v = 1,
+                               lmean = 0,
+                               lsd = 1,
                                phi = -0.75,
                                LN = TRUE) {
+  v <- lsd^2
   sigma <- sqrt(v)
-  sigma_e <- sigma * sqrt(1 - phi ^ 2)
-  rawSimu <- stats::arima.sim(n = N, list(ar = c(phi)), sd = sigma_e) + m
+  sigma_e <- lsd * sqrt(1 - phi ^ 2)
+  rawSimu <- stats::arima.sim(n = N, list(ar = c(phi)), sd = sigma_e) + lmean
   if (LN) {
     exampleSimu <- exp(rawSimu)
   } else{
